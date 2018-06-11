8fbaf09af3c025e04e83e8f43dd4396ad0e2bdbf
peter <0204386@worldunion.com.cn>
2018-06-11 17:27:47 +0800

    2.3   Bean的初始化和销毁

---
+++ 1.点睛Spring 4.x/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch2/prepost/PrePostConfig.java
@@ -0,0 +1,21 @@
+package com.wisely.highlight_spring4.ch2.prepost;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrePostConfig {
+	
+	@Bean(initMethod="init",destroyMethod="destroy") //1 initMethod和destroyMethod指定BeanWayService
+														//类的init和destroy方法在构造之后，Bean销毁之前执行
+	BeanWayService beanWayService(){
+		return new BeanWayService();
+	}
+	
+	@Bean
+	JSR250WayService jsr250WayService(){
+		return new JSR250WayService();
+	}
+
+}
