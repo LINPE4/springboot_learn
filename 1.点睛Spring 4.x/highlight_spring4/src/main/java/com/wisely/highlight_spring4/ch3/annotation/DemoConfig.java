5b012d81b40c496f64cd2e2648fe242cb11b57bd
peter <0204386@worldunion.com.cn>
2018-06-12 11:09:56 +0800

    3.5  组合注解与元注解

---
+++ 1.点睛Spring 4.x/highlight_spring4/src/main/java/com/wisely/highlight_spring4/ch3/annotation/DemoConfig.java
@@ -0,0 +1,7 @@
+package com.wisely.highlight_spring4.ch3.annotation;
+
+@WiselyConfiguration("com.wisely.highlight_spring4.ch3.annotation") //1 使用@WiselyConfiguartion组合注解替代
+                                                                        //@Configuration和@ComponentScan
+public class DemoConfig {
+
+}
