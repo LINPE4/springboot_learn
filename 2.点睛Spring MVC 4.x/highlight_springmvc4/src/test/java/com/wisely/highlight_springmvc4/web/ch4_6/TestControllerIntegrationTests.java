9eb7892d231cf4e474d45588e9480e463312353d
peter <0204386@worldunion.com.cn>
2018-06-12 16:32:35 +0800

    4.4.4  spring mvc的测试

---
+++ 2.点睛Spring MVC 4.x/highlight_springmvc4/src/test/java/com/wisely/highlight_springmvc4/web/ch4_6/TestControllerIntegrationTests.java
@@ -0,0 +1,73 @@
+package com.wisely.highlight_springmvc4.web.ch4_6;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.wisely.highlight_springmvc4.MyMvcConfig;
+import com.wisely.highlight_springmvc4.service.DemoService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MyMvcConfig.class})
+@WebAppConfiguration("src/main/resources") //1 @WebAppConfiguration注解在类上，用来声明加载的ApplicationContext是一
+											  // 个WebApplicationContext。它的属性指定的是Web资源的位置，默认为src/main/webapp,
+											  //本例修改为src/main/resources
+public class TestControllerIntegrationTests {
+	private MockMvc mockMvc; //2
+	
+	@Autowired
+	private DemoService demoService;//3
+	
+	@Autowired 
+	WebApplicationContext wac; //4
+	
+    @Autowired 
+    MockHttpSession session; //5
+    
+    @Autowired 
+    MockHttpServletRequest request; //6
+    
+    @Before //7
+    public void setup() {
+    	mockMvc =
+    			MockMvcBuilders.webAppContextSetup(this.wac).build(); //2 mockMvc模拟MVC对象，通过
+																	// MockMvcBuilders.webAppContextSetup(this.wac).build()初始化
+    	}
+	
+	@Test
+	public void testNormalController() throws Exception{
+		mockMvc.perform(get("/normal")) //8 模拟向/normal进行get请求
+				.andExpect(status().isOk())//9 预期控制返回状态为200
+				.andExpect(view().name("page"))//10 预期view的名称为page
+				.andExpect(forwardedUrl("/WEB-INF/classes/views/page.jsp"))//11 预期页面转向的真正路径
+																							// 为/WEB-INF/classes/views/page.jsp
+				.andExpect(model().attribute("msg", demoService.saySomething()));//12
+				
+	}
+	
+	@Test
+	public void testRestController() throws Exception{
+		mockMvc.perform(get("/testRest")) //13
+        .andExpect(status().isOk())
+         .andExpect(content().contentType("text/plain;charset=UTF-8"))//14
+        .andExpect(content().string(demoService.saySomething()));//15
+	}
+
+}
