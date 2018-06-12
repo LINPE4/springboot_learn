db73d75579681b0211f4abeb34963f294f0f8643
peter <0204386@worldunion.com.cn>
2018-06-12 14:26:37 +0800

    4.1  spring mvc项目快速搭建

---
+++ 2.点睛Spring MVC 4.x/highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -0,0 +1,31 @@
+package com.wisely.highlight_springmvc4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc// 1 EnableWebMvc开启Springmvc支持，若无此句，重写WebMvcConfigurerAdapter方法无效
+@EnableScheduling
+@ComponentScan("com.wisely.highlight_springmvc4")
+public class MyMvcConfig extends WebMvcConfigurerAdapter {// 2 集成WebMvcConfigurerAdapter，重写其方法可对spring mvc进行配置
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+
+
+
+
+
+}
