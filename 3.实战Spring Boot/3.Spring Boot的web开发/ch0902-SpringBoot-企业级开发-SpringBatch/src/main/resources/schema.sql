912de2ce999f154eda91f683db7a1b73c0b1fd05
peter <0204386@worldunion.com.cn>
2018-06-21 11:42:53 +0800

    9.2  SpringBoot-企业级开发-SpringBatch

---
+++ 3.实战Spring Boot/3.Spring Boot的web开发/ch0902-SpringBoot-企业级开发-SpringBatch/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS PERSON (
+  id      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name    NVARCHAR(20),
+  age     INT,
+  nation  NVARCHAR(20),
+  address NVARCHAR(20)
+);
+
