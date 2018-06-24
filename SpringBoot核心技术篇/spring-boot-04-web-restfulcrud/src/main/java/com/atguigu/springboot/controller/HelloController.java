0db39cc3d6988064e52cfaf38259faf55f645e2a
peter <610002997@qq.com>
2018-06-24 23:16:21 +0800

    29、尚硅谷_SpringBoot_web开发-webjars&静态资源映射规则

---
+++ SpringBoot核心技术篇/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public  String hello(@RequestParam("user") String user){
+        return "Hello World";
+    }
+
+
+
+}
