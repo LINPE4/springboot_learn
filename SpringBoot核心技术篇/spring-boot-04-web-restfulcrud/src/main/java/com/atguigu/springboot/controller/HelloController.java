772a6d5935cc50546c16a6603e60e1843fbb815d
peter <610002997@qq.com>
2018-06-28 22:06:56 +0800

    46、尚硅谷_SpringBoot_web开发-嵌入式Servlet容器配置修改

--- SpringBoot核心技术篇/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ SpringBoot核心技术篇/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.atguigu.springboot.controller;
 
 
+import com.atguigu.springboot.exception.UserNotExistException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
@@ -14,7 +16,10 @@ public class HelloController {
 
     @ResponseBody
     @RequestMapping("/hello")
-    public  String hello(){
+    public  String hello(@RequestParam("user") String user){
+        if(user.equals("aaa")){
+            throw new UserNotExistException();
+        }
         return "Hello World";
     }
 
